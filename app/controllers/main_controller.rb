4de364aef544b6b18667ed28ff402e5628c21ba5
Jon Lynch <jonnylynchy@gmail.com>
2012-07-01 16:46:17 -0500

    added form and validation for SiteUser model

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,20 @@
 class MainController < ApplicationController
   def signin
+    
+  end
+
+  def login
+    if /^.+@.+$/.match(params[:email]).nil?
+      flash[:notice] = "Please enter a valid email address"
+      redirect_to :back
+    else
+      @user = SiteUser.find_by_email(params[:email])
+      if !@user.nil?
+        @user = SiteUser.new({:email => params[:email], :ipaddress => request.remote_ip})
+        @user.save
+      end
+      redirect_to video_path
+    end
   end
 
   def video
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 InfopackonlineCom::Application.routes.draw do
   root to: 'main#signin'
   match "/signin", to: 'main#signin'
+  match "/login", to: 'main#login'
   match "/video", to: 'main#video'
 end

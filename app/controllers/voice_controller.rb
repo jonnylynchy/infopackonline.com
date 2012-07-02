a6624c1a21f97037ebbb48186327d7619f5a67b0
Jon Lynch <jonnylynchy@gmail.com>
2012-07-02 18:43:44 -0500

    added twiml contoller for 3 way audio

--- app/controllers/voice_controller.rb
+++ app/controllers/voice_controller.rb
@@ -2,6 +2,6 @@ class VoiceController < ApplicationController
   def three_way_audio
     @audio_url = "http://s3.amazonaws.com/eteam-s3/VisalusSpotTWO.mp3"
     @goodbye_message = "The audio file is complete. Goodbye."
-    render action: 'three_way_audio.xml.builder', layout: false
+    render action: 'three_way_audio.xml.builder'
   end
 end

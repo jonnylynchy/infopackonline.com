028bc26546e2856fe27765b4bf7802bc00081057
Jon Lynch <jonnylynchy@gmail.com>
2012-07-02 17:15:28 -0500

    fixed logic for signin, adjusted admin table layout

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -4,16 +4,18 @@ class MainController < ApplicationController
   end
 
   def login
-    if /^.+@.+$/.match(params[:email]).nil?
-      flash[:notice] = "Please enter a valid email address"
-      redirect_to :back
-    else
+    # if valid email, show video
+    if /^.+@.+$/.match(params[:email])
       @user = SiteUser.find_by_email(params[:email])
       if @user.nil?
         @user = SiteUser.new({:email => params[:email], :ipaddress => request.remote_ip})
         @user.save
       end
       redirect_to video_path
+    else
+    # if not valid email, show error
+      flash[:notice] = "Please enter a valid email address"
+      redirect_to :back
     end
   end
 
